3bdd3f4c8251b799296dc8447265ce73149d34b1
Freilin Jose <freilinjb@gmail.com>
2019-06-30 23:48:34 -0400

    wef

--- NODEJS - PRACTICAS/NodeServer/src/index.js
+++ NODEJS - PRACTICAS/NodeServer/src/index.js
@@ -1,45 +1,87 @@
 var express = require('express');
 var sql = require('mssql');
-var app = express();
-
-// app.get('/', function (req, res) {
-
-
-//     // config for your database
-//     var config = {
-//         user: 'freilinjb',
-//         password: 'fre123i4',
-//         server: 'DESKTOP-0GJ4POO\\SQLEXPRESS',
-//         database: 'Proyecto'
-//     };
-
-//     // connect to your database
-//     sql.connect(config, function (err) {
-
-//         if (err) console.log(err);
-
-//         // create Request object
-//         var request = new sql.Request();
-
-
 const app = express();
 
-app.set('port',process.env.PORT || 3000 );
-
-//Middlewares
-app.use(morgan('dev'));
+app.get('/', function (req, res) {
 
 
-app.get('/', (req,res) =>{
-    res.send('Hola Mundo');
+    // config for your database
+    var config = {
+        user: 'freilinjb',
+        password: 'fre123i4',
+        server: 'DESKTOP-0GJ4POO\\SQLEXPRESS',
+        database: 'Proyecto'
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+
+        // query to the database and get the records
+        request.query('select top 3 id_tercero,nombre from VistaAbogado', function (err, recordset) {
+
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+
+        });
+    });
 });
 
-app.get('/', function() {
-    var config = {
-        user : 
-    }
-})
+var server = app.listen(3000, function () {
+    console.log('Server is running..');
+});
 
-app.listen(app.get('port'), ()=> {
-    console.log(`Server on port ${app.get('port')}`);
-})
+// const express = require('express');
+// path = require('path');
+// morgan = require('morgan');
+// sql = require('mssql');
+
+// const app = express();
+
+// app.set('port',process.env.PORT || 3000 );
+
+// //Middlewares
+// app.use(morgan('dev'));
+
+
+// // app.get('/', (req,res) =>{
+// //     res.send('Hola Mundo');
+// // });
+
+// app.get('/', function(req, res) {
+//     var config = {
+//         user : 'freilinjb',
+//         password : 'fre123i4',
+//         server: 'DESKTOP-0GJ4POO\SQLEXPRESS',
+//         database: 'Proyecto'
+//     }
+
+//     //Connect to your database
+//     sql.connect(config, function(err){
+//         if(err)
+//             console.log(err);
+
+//         var request = new sql.Request();
+
+//         request.query('select * from VistaAbogado ', function(err, recordset) {
+
+//             if(err)
+//                 console.log(err);
+
+//             res.send(recordset);
+
+//         })
+
+//     })
+// })
+
+// app.listen(app.get('port'), ()=> {
+//     console.log(`Server on port ${app.get('port')}`);
+// })
